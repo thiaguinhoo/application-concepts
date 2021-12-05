55498048889db9eb45641129717fbac6b70cd2be
thiaguinhoo <thiagoluizsog@gmail.com>
2021-12-05 12:58:35 -0300

    add validation environment and .env

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,5 +1,10 @@
+import 'dotenv/config';
 import express from 'express';
 
 const app = express();
+
+app.get('/', async (_req, res) => {
+  res.json({ message: 'Welcome to development world!' });
+});
 
 export default app;
--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,4 +1,8 @@
-import app from "./app";
+import { cleanEnv, num } from 'envalid';
+
+import app from './app';
+
+cleanEnv(process.env, { SERVER_PORT: num() });
 
 const serverPort = process.env.SERVER_PORT || 3333;
 
